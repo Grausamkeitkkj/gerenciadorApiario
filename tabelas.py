728ceb56b2b801b6381d6d616bdd7a2c51651d5f
grausamkeitkkj <diogorschultz2@gmail.com>
2024-06-24 15:02:28 -0300

    colocando try except para pegar possíveis erros na criação das tabelas

--- tabelas.py
+++ tabelas.py
@@ -2,26 +2,20 @@ import psycopg2
 import conector
 
 def criar_tabelas():
-    conn = conector.conn
-    cursor = conn.cursor()
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS caixas (
-            id_caixa SERIAL PRIMARY KEY,
-            nome_caixa VARCHAR(50) NOT NULL,
-            tipo_caixa VARCHAR(50) NOT NULL,
-            data_aquisicao DATE NOT NULL,
-            data_instalacao DATE NOT NULL,
-            data_retirada DATE
-        )
-    """)
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS caixas_abelhas (
-            id_caixa_abelha SERIAL PRIMARY KEY,
-            id_caixa INTEGER NOT NULL,
-            id_abelha INTEGER NOT NULL
-        );
-    """)
-    conn.commit()
-    cursor.close()
-    conn.close()
-    print("Tabela caixas criada com sucesso!")
+    try:
+        conn = conector.conn
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS caixas (
+            )
+        """)
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS abelhas (
+            );
+        """)
+        conn.commit()
+        cursor.close()
+        conn.close()
+        print("Tabelas criadas com sucesso.")
+    except Exception as e:
+        print(f"Erro ao criar tabelas: {e}")
